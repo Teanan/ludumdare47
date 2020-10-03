c59104a68bd682294196dfe36dece41360b2fb14
lbonnal <lucas.bonnal@gmail.com>
2020-10-03 12:18:12 +0200

    :sparkles: This, is a killer bucket.

--- Elements/Bucket.gd
+++ Elements/Bucket.gd
@@ -1,7 +1,11 @@
 extends StaticBody2D
 
+signal ballEntered
+
 func _ready():
 	pass # Replace with function body.
 
 func _on_Area_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "Ball":
+		emit_signal("ballEntered", body)
+		body.queue_free()
