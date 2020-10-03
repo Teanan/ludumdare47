249c8c6b7cbbc3ee8d003c7cc5b60453e2b8c4c1
lbonnal <lucas.bonnal@gmail.com>
2020-10-03 13:02:19 +0200

    This, is a killerer bucket

--- Elements/Bucket.gd
+++ Elements/Bucket.gd
@@ -6,6 +6,6 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Area_body_entered(body):
-	if body.name == "Ball":
+	if body.is_in_group("balls"):
 		emit_signal("ballEntered", body)
 		body.queue_free()
