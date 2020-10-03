d042d20d05aa6e2104aa2a876e9f8a2b91987265
Tom Spreters <tom.spreters@gmail.com>
2020-10-03 19:21:45 +0200

    :loud_sound:

--- Elements/Ball.gd
+++ Elements/Ball.gd
@@ -1,10 +1,6 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var velocity: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,5 +10,12 @@ func _free():
 	self.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	velocity = linear_velocity
+
+func _on_Ball_body_entered(body):
+	if (linear_velocity - velocity).length() > 150:
+		var bar_notes = ChefOrchestre.notes[ChefOrchestre.bar - 1]
+		var note = bar_notes[rand_range(0,bar_notes.size())]
+		$AudioStreamPlayer2D.play()
+		$AudioStreamPlayer2D.pitch_scale = ChefOrchestre.pitch[note]
